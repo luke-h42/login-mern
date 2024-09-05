466941a0885b5346b6d785e2d0298a6dcbe96174
Luke Hancock <lukehancock42@gmail.com>
2024-09-05 23:00:40 +0100

    reverted cookie expiration maxage

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -70,7 +70,7 @@ export const loginUser = async (req, res) => {
         if(match) {
             jwt.sign({email: user.email, id: user._id, name: user.name}, process.env.JWT_SECRET, {}, (err, token) => {
                 if(err) throw err;
-                res.cookie('token', token, {httpOnly: true, sameSite: 'None', secure: true, path: "/", expires: new Date(0)}).json(user);
+                res.cookie('token', token, {httpOnly: true, sameSite: 'None', secure: true, path: "/", maxAge: 3600 * 1000}).json(user);
             })
 
         }
