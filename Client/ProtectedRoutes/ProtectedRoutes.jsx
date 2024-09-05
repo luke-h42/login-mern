00fed7a1a36cd6849ad90810b7967a571cce0712
Luke Hancock <lukehancock42@gmail.com>
2024-09-05 23:30:30 +0100

    loading bugfix revert

--- Client/ProtectedRoutes/ProtectedRoutes.jsx
+++ Client/ProtectedRoutes/ProtectedRoutes.jsx
@@ -4,37 +4,22 @@ import axios from 'axios';
 
 export default function ProtectedRoutes() {
   const [isAuthenticated, setIsAuthenticated] = useState(null);
-  const [showLoading, setShowLoading] = useState(true);
 
   useEffect(() => {
-    // Simulate delay for loading state visibility
-    const loadingTimeout = setTimeout(() => {
-      setShowLoading(false);
-    }, 500);
 
-    // Check authentication
     axios.get('/authentication', { withCredentials: true })
       .then(response => {
         setIsAuthenticated(true);
       })
       .catch(error => {
         setIsAuthenticated(false);
-      })
-      .finally(() => {
-        // Clear timeout if authentication check is complete
-        clearTimeout(loadingTimeout);
       });
   }, []);
 
-  if (showLoading) {
-    // Display loading state while waiting for authentication check
-    return (
-      <div className="flex items-center justify-center flex-col h-[calc(100vh-40px)] bg-white dark:bg-gray-900">
-        <div className="text-5xl text-gray-700 dark:text-gray-300">Loading...</div>
-      </div>
-    );
+  if (isAuthenticated === null) {
+    // Loading state while checking authentication
+    return <div className="flex items-center justify-center flex-col h-[calc(100vh-40px)] bg-white dark:bg-gray-900">Loading...</div>;
   }
 
-  // After loading state, render based on authentication status
   return isAuthenticated ? <Outlet /> : <Navigate to="/login" />;
 }
