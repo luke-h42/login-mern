637cd75c6c755c6723db1921730b4c373ff67857
Luke Hancock <lukehancock42@gmail.com>
2024-09-05 19:29:55 +0100

    cookie bugfix 2

--- Server/Controllers/authController.js
+++ Server/Controllers/authController.js
@@ -109,6 +109,6 @@ export const getProfile = (req,res) => {
 }
 
 export const logoutUser = (req, res) => {
-    res.clearCookie('token', {httpOnly: true, sameSite: 'None', secure: true,  path: "/"})
+    res.clearCookie('token', {httpOnly: true, sameSite: 'None', secure: true,  path: "/", expires: new Date(0)})
     res.status(200).json({message: 'Logged out successfully'})
 }
