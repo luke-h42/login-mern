90657419197e56a748b5468c4e9dfbd1e32c4502
Luke Hancock <lukehancock42@gmail.com>
2024-09-05 12:01:26 +0100

    changed backend urls to work with render

--- Server/Routes/authRoutes.js
+++ Server/Routes/authRoutes.js
@@ -7,7 +7,8 @@ const router = express.Router();
 router.use(
     cors({
         credentials: true,
-        origin: 'http://localhost:5173'
+        //origin: 'http://localhost:5173'
+        origin: 'https://login-mern-1.onrender.com'
     })
 )
 
