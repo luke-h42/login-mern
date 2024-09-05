17b501c8f1727084465ed6b04f7506997e105fd1
Luke Hancock <lukehancock42@gmail.com>
2024-09-06 00:59:32 +0100

    updated to link over a tags

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import cookies from 'js-cookie'
 import toast from 'react-hot-toast'
 import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import { useContext } from "react"
 import {UserContext} from "../../context/userContext"
 
@@ -25,15 +25,15 @@ export default function Navbar() {
     <nav className="flex justify-center bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300">
       <div className='flex w-8/12 px-2 py-2 items-center justify-between' >
       
-        <a href='/' >Home</a>
+        <Link to='/' >Home</Link>
         {!user ? (
           <>
-            <a href='/login'>Login</a>
-            <a href='/register'>Register</a>
+            <Link to='/login'>Login</Link>
+            <Link to='/register'>Register</Link>
           </>
         ) : (
           <>
-            <a href='/dashboard'>Dashboard</a>
+            <Link to='/dashboard'>Dashboard</Link>
             <button onClick={handleSignOut}>Sign Out</button>
           </>
         )}
