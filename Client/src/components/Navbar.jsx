2c853674f83416809fa079bab70ac81847184b97
Luke Hancock <lukehancock42@gmail.com>
2024-09-06 00:55:41 +0100

    navbar update to ternary operator

--- Client/src/components/Navbar.jsx
+++ Client/src/components/Navbar.jsx
@@ -24,17 +24,19 @@ export default function Navbar() {
   return (
     <nav className="flex justify-center bg-white dark:bg-gray-900 text-gray-700 dark:text-gray-300">
       <div className='flex w-8/12 px-2 py-2 items-center justify-between' >
-      {!user && <>
-        <a href='/' >Home</a>
-        <a href='/login' >Login</a>
-        <a href='/register' >Register</a>
-      </>}
       
-        {!!user && (<>
-          <a href='/' >Home</a>
-          <a href='/dashboard' >Dashboard</a>
-          <button onClick={handleSignOut}>Sign Out</button>
-        </>)}
+        <a href='/' >Home</a>
+        {!user ? (
+          <>
+            <a href='/login'>Login</a>
+            <a href='/register'>Register</a>
+          </>
+        ) : (
+          <>
+            <a href='/dashboard'>Dashboard</a>
+            <button onClick={handleSignOut}>Sign Out</button>
+          </>
+        )}
        
       </div>
     </nav>
